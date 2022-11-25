f69361dd09171f055a8bd73457e53dde6cdb7517
Eroica <seba79@mailbox.org>
2022-11-25 10:05:18 +0100

    Remove smooth scrolling from JS
    This can be done in CSS by now.

--- js/main.js
+++ js/main.js
@@ -3,148 +3,53 @@
 
 "use strict";
 
-// easing functions http://goo.gl/5HLl8
-Math.easeInOutQuad = function (t, b, c, d) {
-	t /= d/2;
-	if (t < 1) {
-		return c/2*t*t + b
-	}
-	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
-};
-
-Math.easeInCubic = function(t, b, c, d) {
-	var tc = (t/=d)*t*t;
-	return b+c*(tc);
-};
-
-Math.inOutQuintic = function(t, b, c, d) {
-	var ts = (t/=d)*t,
-	tc = ts*t;
-	return b+c*(6*tc*ts + -15*ts*ts + 10*tc);
-};
-
-
-function scrollTo(to, callback, duration) {
-	function move(amount) {
-		document.documentElement.scrollTop = amount;
-		document.body.parentNode.scrollTop = amount;
-		document.body.scrollTop = amount;
-	}
-	function position() {
-		return document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
-	}
-	var start = position(),
-	change = to - start,
-	currentTime = 0,
-	increment = 20;
-	duration = (typeof(duration) === 'undefined') ? 500 : duration;
-	var animateScroll = function() {
-		// increment the time
-		currentTime += increment;
-		// find the value with the quadratic in-out easing function
-		var val = Math.easeInOutQuad(currentTime, start, change, duration);
-		// move the document.body
-		move(val);
-		// do the animation unless its over
-		if (currentTime < duration) {
-			requestAnimationFrame(animateScroll);
-		} else {
-			if (callback && typeof(callback) === 'function') {
-				// the animation is done so lets callback
-				callback();
-			}
-		}
-	};
-	animateScroll();
-}
-
 (function() {
-	var anchors = document.getElementsByTagName("a");
-	var local_anchors = [];
-
-	for(var i = 0; i < anchors.length; i++) {
-		if(    anchors[i].attributes.href.value.indexOf("#") > -1
-				&& anchors[i].attributes.href.value !== "#"
-				&& anchors[i].attributes.href.value !== "#nav") {
-			local_anchors.push(anchors[i]);
-		}
-	}
-
-	for(var i = 0; i < local_anchors.length; i++) {
-		var hash_position = local_anchors[i].attributes.href.value.indexOf("#");
-		var sub_string = local_anchors[i].attributes.href.value.substring(hash_position);
-		var target = document.getElementById(sub_string.substring(1));
-
-		local_anchors[i].addEventListener("click", function(target) {
-			return function() { scrollTo(target.offsetTop); }
-		}(target))
-	}
-})();
-
-
-
-(function() {
-	var	$body = document.querySelector('body'),
+	const $body = document.querySelector('body'),
 		$banner = document.querySelector('.banner'),
 		$nav = document.querySelector('.header-nav');
-
-	var $snackbar = document.querySelector(".snackbar");
-
-	if ($snackbar != null) {
-		document.querySelector(".snackbar > .dismiss-btn").addEventListener("click", function () {
-				$snackbar.classList.remove("visible");
-			});
-	}
 
 	// Disable animations/transitions until everything's loaded.
 	$body.classList.add('is-loading');
 
 	window.addEventListener('load', function() {
 		$body.classList.remove('is-loading');
-		if ($snackbar != null) $snackbar.classList.add("visible");
 	});
 
-	var $nav__toggle = document.querySelector('[href="#nav"]');
-	$nav__toggle.addEventListener("click", function () {
-		$nav.classList.toggle("header-nav--expanded");
-	})
-
-	var locked = false;
+	let locked = false;
 
 	window.addEventListener("scroll", function () {
-		if(locked) return;
+		if (locked) {
+			return;
+		}
 
 		locked = true;
 
-		var height = $banner.getBoundingClientRect().height;
-		var top = $banner.getBoundingClientRect().top;
+		const height = $banner.getBoundingClientRect().height;
+		const top = $banner.getBoundingClientRect().top;
 
-		if(Math.abs(top) < height) {
-			$nav.classList.remove("header-nav--visible");
+		if (Math.abs(top) < height) {
+			$nav.classList.remove("-visible");
 		}
-		if(Math.abs(top) > height) {
-			$nav.classList.add("header-nav--visible");
+		if (Math.abs(top) > height) {
+			$nav.classList.add("-visible");
 		}
 
 		locked = false;
 	});
 
-
 	// Change Download button depending on operating system.
-	var $button_icon = document.querySelector("[data-button='download'] > i");
-	var $button = document.querySelector("[data-button='download']")
+	const $button_icon = document.querySelector("[data-button='download'] > i");
+	const $button = document.querySelector("[data-button='download']")
 
-	if(navigator.userAgent.indexOf("Macintosh") > -1) {
+	if (navigator.userAgent.indexOf("Macintosh") > -1) {
 		$button_icon.classList.remove("fa-windows");
 		$button_icon.classList.add("fa-apple");
 		$button.setAttribute("href", "{{ site.data.knossos.mac-url }}");
 	}
 
-	if(navigator.userAgent.indexOf("Linux") > -1) {
+	if (navigator.userAgent.indexOf("Linux") > -1) {
 		$button_icon.classList.remove("fa-windows");
 		$button_icon.classList.add("fa-linux");
 		$button.setAttribute("href", "{{ site.data.knossos.linux-url }}");
 	}
 })();
-
